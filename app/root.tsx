592d1754fd3aadf5ba92aa75fc907d2d62f92a59
Zack Umar <zackumar2@gmail.com>
2023-03-25 17:26:01 -0500

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import type { LinksFunction, MetaFunction } from '@remix-run/cloudflare';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+
+import stylesheet from '~/styles/tailwind.css';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+];
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export default function App() {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full font-sans">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
