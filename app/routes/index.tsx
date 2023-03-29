d0b93c3aad9e25760776ade7e66583d9ca459af7
isabelle molinas <molinasisabelle@gmail.com>
2023-03-29 18:39:58 -0500

    UI - location marker, logo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -114,15 +114,15 @@ export default function Index() {
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        const svgMarker = {
-          path: 'M25 50C38.8071 50 50 38.8071 50 25C50 11.1929 38.8071 0 25 0C11.1929 0 0 11.1929 0 25C0 38.8071 11.1929 50 25 50ZM25.5 37C32.4036 37 38 31.4036 38 24.5C38 17.5964 32.4036 12 25.5 12C18.5964 12 13 17.5964 13 24.5C13 31.4036 18.5964 37 25.5 37Z',
-          fillColor: '#ff0000',
-          fillOpacity: 0.9,
-          strokeWeight: 0,
-          rotation: 0,
-          scale: 0.5,
-          anchor: new google.maps.Point(15, 30),
-        };
+        // const svgMarker = {
+        //   path: 'M25 50C38.8071 50 50 38.8071 50 25C50 11.1929 38.8071 0 25 0C11.1929 0 0 11.1929 0 25C0 38.8071 11.1929 50 25 50ZM25.5 37C32.4036 37 38 31.4036 38 24.5C38 17.5964 32.4036 12 25.5 12C18.5964 12 13 17.5964 13 24.5C13 31.4036 18.5964 37 25.5 37Z',
+        //   fillColor: '#7079b9',
+        //   fillOpacity: 1,
+        //   strokeWeight: 0,
+        //   rotation: 0,
+        //   scale: 0.4,
+        //   anchor: new google.maps.Point(15, 30),
+        // };
 
         map.panTo(
           new goo.maps.LatLng(
@@ -136,7 +136,11 @@ export default function Index() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           },
-          icon: svgMarker,
+          icon: {
+            url: "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg id='Layer_2' data-name='Layer 2' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Cdefs%3E%3Cstyle%3E .cls-1 %7B fill: %23818CF8; %7D .cls-2 %7B fill: %23fff; %7D %3C/style%3E%3C/defs%3E%3Cg id='Layer_1-2' data-name='Layer 1'%3E%3Ccircle class='cls-2' cx='50' cy='50' r='50'/%3E%3Ccircle class='cls-1' cx='50' cy='50' r='40'/%3E%3C/g%3E%3C/svg%3E",
+            scaledSize: new google.maps.Size(30, 30)
+          },
+          // svgMarker,
           map: map,
         });
       });
