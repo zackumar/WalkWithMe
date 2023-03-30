d9ca0ab76053384e0f6720c5eaadd64c6ed2b0e6
Zachary True <zacharytrue13@gmail.com>
2023-03-30 13:49:22 -0500

    Add buddy live location

--- app/routes/buddy.tsx
+++ app/routes/buddy.tsx
@@ -37,6 +37,35 @@ export default function BuddySystem() {
     setBuddyLocation([location.coords.latitude, location.coords.longitude]);
   }, [location]);
 
+  const [marker, setMarker] = useState<google.maps.Marker>();
+
+  useEffect(() => {
+    if (!goo || !map || !location) return;
+    if (!marker) {
+
+      setMarker(
+        new google.maps.Marker({
+          position: {
+            lat: location.coords.latitude,
+            lng: location.coords.longitude,
+          },
+          icon: {
+            url: "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg id='Layer_2' data-name='Layer 2' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Cdefs%3E%3Cstyle%3E .cls-1 %7B fill: %23818CF8; %7D .cls-2 %7B fill: %23fff; %7D %3C/style%3E%3C/defs%3E%3Cg id='Layer_1-2' data-name='Layer 1'%3E%3Ccircle class='cls-2' cx='50' cy='50' r='50'/%3E%3Ccircle class='cls-1' cx='50' cy='50' r='40'/%3E%3C/g%3E%3C/svg%3E",
+            scaledSize: new google.maps.Size(30, 30)
+          },
+          map: map,
+        })
+      );
+    }
+
+    if (marker) {
+      marker.setPosition({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    }
+  }, [location, goo, map, marker]);
+
   const [routeId, setRouteId] = useState('');
   const [directionsRenderer, setDirectionsRenderer] =
     useState<google.maps.DirectionsRenderer>();
